fc82f8c7de85a20fc52415ec717dfe3f6f6e225c
andreas <andreas@172.29.251.186>
2017-07-27 15:20:26 +0200

    Added Texts Datamodell and Check

---
+++ Check/src/check/CheckMain.java
@@ -0,0 +1,12 @@
+package check;
+
+import CP.TextsText.*;
+
+
+public class CheckMain {
+	public static void main(String[] args) {
+				
+	System.out.println("CheckMain");
+	TextsText_Text t=TextsText.Check();
+	}	
+}
